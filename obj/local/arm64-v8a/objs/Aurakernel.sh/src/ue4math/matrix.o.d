cb7224f605ace727c5ff9af730e4b2353eb98f0e
xiaon <45096653@qq.com>
2026-06-01 18:35:00 +0800

    优化盒内物资

--- obj/local/arm64-v8a/objs/Aurakernel.sh/src/ue4math/matrix.o.d
+++ obj/local/arm64-v8a/objs/Aurakernel.sh/src/ue4math/matrix.o.d
@@ -1,14 +1,14 @@
-D:/文档/GitHub/via/obj/local/arm64-v8a/objs/Aurakernel.sh/src/ue4math/matrix.o: \
-  D:/文档/GitHub/via/jni/src/ue4math/matrix.cpp \
-  D:/文档/GitHub/via/jni/include/matrix.h \
-  D:/文档/GitHub/via/jni/include/ue4math.h \
-  D:/文档/GitHub/via/jni/include/vector.h \
-  D:/文档/GitHub/via/jni/include/rotator.h \
-  D:/文档/GitHub/via/jni/include/transform.h \
-  D:/文档/GitHub/via/jni/include/quat.h
-D:/文档/GitHub/via/jni/include/matrix.h:
-D:/文档/GitHub/via/jni/include/ue4math.h:
-D:/文档/GitHub/via/jni/include/vector.h:
-D:/文档/GitHub/via/jni/include/rotator.h:
-D:/文档/GitHub/via/jni/include/transform.h:
-D:/文档/GitHub/via/jni/include/quat.h:
+C:/Users/园中/Documents/GitHub/via/obj/local/arm64-v8a/objs/Aurakernel.sh/src/ue4math/matrix.o: \
+  C:/Users/园中/Documents/GitHub/via/jni/src/ue4math/matrix.cpp \
+  C:/Users/园中/Documents/GitHub/via/jni/include/matrix.h \
+  C:/Users/园中/Documents/GitHub/via/jni/include/ue4math.h \
+  C:/Users/园中/Documents/GitHub/via/jni/include/vector.h \
+  C:/Users/园中/Documents/GitHub/via/jni/include/rotator.h \
+  C:/Users/园中/Documents/GitHub/via/jni/include/transform.h \
+  C:/Users/园中/Documents/GitHub/via/jni/include/quat.h
+C:/Users/园中/Documents/GitHub/via/jni/include/matrix.h:
+C:/Users/园中/Documents/GitHub/via/jni/include/ue4math.h:
+C:/Users/园中/Documents/GitHub/via/jni/include/vector.h:
+C:/Users/园中/Documents/GitHub/via/jni/include/rotator.h:
+C:/Users/园中/Documents/GitHub/via/jni/include/transform.h:
+C:/Users/园中/Documents/GitHub/via/jni/include/quat.h:
