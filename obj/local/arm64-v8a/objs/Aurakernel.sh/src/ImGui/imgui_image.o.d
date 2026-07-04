52d301e0ba82a7b270c14d7858f74fdc885fa4e0
xiaon <45096653@qq.com>
2026-07-04 19:58:40 +0800

    优化

--- obj/local/arm64-v8a/objs/Aurakernel.sh/src/ImGui/imgui_image.o.d
+++ obj/local/arm64-v8a/objs/Aurakernel.sh/src/ImGui/imgui_image.o.d
@@ -1,10 +1,10 @@
-D:/文档/GitHub/via/obj/local/arm64-v8a/objs/Aurakernel.sh/src/ImGui/imgui_image.o: \
-  D:/文档/GitHub/via/jni/src/ImGui/imgui_image.cpp \
-  D:/文档/GitHub/via/jni/include/ImGui/imgui_image.h \
-  D:/文档/GitHub/via/jni/include/ImGui/imgui.h \
-  D:/文档/GitHub/via/jni/include/ImGui/imconfig.h \
-  D:/文档/GitHub/via/jni/include/ImGui/stb_image.h
-D:/文档/GitHub/via/jni/include/ImGui/imgui_image.h:
-D:/文档/GitHub/via/jni/include/ImGui/imgui.h:
-D:/文档/GitHub/via/jni/include/ImGui/imconfig.h:
-D:/文档/GitHub/via/jni/include/ImGui/stb_image.h:
+C:/Users/园中/Documents/GitHub/via/obj/local/arm64-v8a/objs/Aurakernel.sh/src/ImGui/imgui_image.o: \
+  C:/Users/园中/Documents/GitHub/via/jni/src/ImGui/imgui_image.cpp \
+  C:/Users/园中/Documents/GitHub/via/jni/include/ImGui/imgui_image.h \
+  C:/Users/园中/Documents/GitHub/via/jni/include/ImGui/imgui.h \
+  C:/Users/园中/Documents/GitHub/via/jni/include/ImGui/imconfig.h \
+  C:/Users/园中/Documents/GitHub/via/jni/include/ImGui/stb_image.h
+C:/Users/园中/Documents/GitHub/via/jni/include/ImGui/imgui_image.h:
+C:/Users/园中/Documents/GitHub/via/jni/include/ImGui/imgui.h:
+C:/Users/园中/Documents/GitHub/via/jni/include/ImGui/imconfig.h:
+C:/Users/园中/Documents/GitHub/via/jni/include/ImGui/stb_image.h:
